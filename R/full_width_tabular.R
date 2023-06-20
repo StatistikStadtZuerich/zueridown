f5e61cce5da8fc05ae3486b47b7786d9b94ab4ae
angelicambg <angelicambg@gmail.com>
2023-06-20 10:21:27 +0200

    full width functionality

---
+++ R/full_width_tabular.R
@@ -0,0 +1,13 @@
+#' @export
+full_width_tabular <- function(x) {
+  x <- str_replace_all(x, fixed("{tabu}"), "{tabularx}")
+  x <- str_replace_all(x, fixed(" to \\linewidth "), "{\\linewidth}")
+  x <- str_replace_all(x, fixed("raggedleft"), "raggedleft\\arraybackslash")
+
+  x <- str_replace_all(x, fixed("raggedright"), "raggedright\\arraybackslash")
+
+  #FIXME: centering
+  x <- str_replace_all(x, fixed("centering"), "centering\\arraybackslash")
+
+  return(cat(x[1]))
+}
