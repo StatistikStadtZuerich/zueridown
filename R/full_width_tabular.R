0431403d985f9e49f525fd46f43bdc9e0f1a0a13
Hänzi Sara (SSZ) <sara.haenzi@zuerich.ch>
2026-06-01 11:22:48 +0200

    run styler

--- R/full_width_tabular.R
+++ R/full_width_tabular.R
@@ -1,13 +1,33 @@
 #' @export
 full_width_tabular <- function(x) {
-  x <- str_replace_all(x, fixed("{tabu}"), "{tabularx}")
-  x <- str_replace_all(x, fixed(" to \\linewidth "), "{\\linewidth}")
-  x <- str_replace_all(x, fixed("raggedleft"), "raggedleft\\arraybackslash")
+  x <- str_replace_all(
+    x,
+    fixed("{tabu}"),
+    "{tabularx}"
+  )
+  x <- str_replace_all(
+    x,
+    fixed(" to \\linewidth "),
+    "{\\linewidth}"
+  )
+  x <- str_replace_all(
+    x,
+    fixed("raggedleft"),
+    "raggedleft\\arraybackslash"
+  )
 
-  x <- str_replace_all(x, fixed("raggedright"), "raggedright\\arraybackslash")
+  x <- str_replace_all(
+    x,
+    fixed("raggedright"),
+    "raggedright\\arraybackslash"
+  )
 
-  #FIXME: centering
-  x <- str_replace_all(x, fixed("centering"), "centering\\arraybackslash")
+  # FIXME: centering
+  x <- str_replace_all(
+    x,
+    fixed("centering"),
+    "centering\\arraybackslash"
+  )
 
   return(cat(x[1]))
 }
