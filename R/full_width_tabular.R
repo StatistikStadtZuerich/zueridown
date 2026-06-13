864aa8532779feb0b699e9f591f2c95f19a2cea2
Kirill Müller <kirill@cynkra.com>
2026-06-13 17:04:31 +0200

    chore: Adjust dependencies, remove stringr and methods

--- R/full_width_tabular.R
+++ R/full_width_tabular.R
@@ -1,33 +1,12 @@
 #' @export
 full_width_tabular <- function(x) {
-  x <- str_replace_all(
-    x,
-    fixed("{tabu}"),
-    "{tabularx}"
-  )
-  x <- str_replace_all(
-    x,
-    fixed(" to \\linewidth "),
-    "{\\linewidth}"
-  )
-  x <- str_replace_all(
-    x,
-    fixed("raggedleft"),
-    "raggedleft\\arraybackslash"
-  )
-
-  x <- str_replace_all(
-    x,
-    fixed("raggedright"),
-    "raggedright\\arraybackslash"
-  )
+  x <- gsub("{tabu}", "{tabularx}", x, fixed = TRUE)
+  x <- gsub(" to \\linewidth ", "{\\linewidth}", x, fixed = TRUE)
+  x <- gsub("raggedleft", "raggedleft\\arraybackslash", x, fixed = TRUE)
+  x <- gsub("raggedright", "raggedright\\arraybackslash", x, fixed = TRUE)
 
   # FIXME: centering
-  x <- str_replace_all(
-    x,
-    fixed("centering"),
-    "centering\\arraybackslash"
-  )
+  x <- gsub("centering", "centering\\arraybackslash", x, fixed = TRUE)
 
-  return(cat(x[1]))
+  cat(x[1])
 }
